2343aa69df993eddcb33f92920022f44b32fe886
vargasjuanibarra <vargasjuanibarra@gmail.com>
2024-07-03 18:31:55 +0800

    fix user experience

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,22 @@
       <Header />
       <Main></Main>
       <Projects id="projects"/>
-      <AboutMe id="aboutme"/>
-      <Experience  id="experience"/>
+      <Highlight id="highlight"/>
+      <Experience id="experience"/>
     </div>
     <Footer id="contact"/>
   </div>
 </template>
+
 <script setup>
 import Header from './components/Header.component.vue';
 import Main from './components/Main.component.vue';
 import Projects from './components/Projects.component.vue';
-import AboutMe from './components/About.component.vue';
+import Highlight from './components/About.component.vue';
 import Experience from './components/Experience.component.vue';
 import Footer from './components/Footer.component.vue';
-
 </script>
+
 <style lang="">
-  
+/* Add your styles here if needed */
 </style>
